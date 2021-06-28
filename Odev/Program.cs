a98968609ba4b092b05744d59c655a0d3176ba64
Beyza Kesler <beyzakslr@hotmail.com>
2021-06-28 14:46:20 +0300

    Add project files.

---
+++ Odev/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            isimler.Add("Beyza");
+
+          
+
+            Console.ReadLine();
+;        }
+      
+    }
+
+    
+}
